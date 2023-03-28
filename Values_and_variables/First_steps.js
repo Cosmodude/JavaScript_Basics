05e270a3b2c1840b935926bdeccb36ab0de0e543
Hackdady <lenskii.vm19@physics.msu.ru>
2023-03-28 13:02:19 +0900

    strings finished

--- Values_and_variables/First_steps.js
+++
@@ -1,9 +0,0 @@
-console.log("HI");
-// numbers
-console.log(35 + 34.654);
-let name = "Vlad";
-let age = 22;
-name = name + "d";
-console.log(name.length);
-console.log(name[-1]);
-
---
+++ Values_and_variables/variables.js
@@ -0,0 +1,18 @@
+console.log("HI");
+// numbers
+console.log(35 + 34.654);
+let name = "Vladislav";
+let age = 22;
+name = name + "d";
+console.log(name.length);
+console.log(name[-1]);
+let index = name.indexOf("i");
+console.log(index);
+let shortName = name.slice(0, name.indexOf("i"));
+console.log(shortName);
+name = name.replace("vd", "v")
+console.log(name);
+let sentence = `I can\'t do it ${index} times`
+console.log(sentence);
+
+
