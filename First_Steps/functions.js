cfc7582fb2aa6d515a74670132bd9c23397869bb
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-02 22:03:29 +0900

    updated functions

--- First_Steps/functions.js
+++ First_Steps/functions.js
@@ -1,11 +1,13 @@
 //alert("Hi");
-let index = 5;
-let numb = Math.pow(index, Math.round(5 * Math.random()))
-let s = `number is ${numb}`;
+function set_number() {
+    let index = 5;
+    let numb = Math.pow(index, Math.round(5 * Math.random()))
+    let s = `number is ${numb}`;
+    return s;
+}
 //console.log(s);
 function reverseString(str) {
     return str.split("").reverse().join("");
 }
-reverseString("hello");
-console.log(parseFloat(reverseString(s)));
+console.log(parseFloat(reverseString(set_number())));
 console.error('Work Harder!!!');
