3f096f8b99d07508b2aac8a737693168e8b61113
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-03 11:46:28 +0900

    for loops finished

--- First_Steps/for_loops.js
+++ First_Steps/for_loops.js
@@ -1,5 +1,19 @@
 for (let i = 0; i < 10; i += 1)
 //   initial exp; condition; increment exp
 {
-    console.log(i);
+    i++;
 }
+
+const rare = new Array(1, 2, 34, 5);
+for (let i = 0; i < rare.length; i++){
+    console.log(i, rare[i]);
+}
+
+const str = "desserts";
+let reverse = "don't be ";
+console.log(reverse);
+for (let i = str.length-1; i >= 0; i--) {
+    reverse += str[i];
+    console.log(reverse);
+}
+console.log(reverse);
