4bd4aa80e016c27e1445f5da1156f1853bafa097
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-05 12:12:36 +0900

    studied high order functions

---
+++ Second_Step/advanced_func.js
@@ -0,0 +1,51 @@
+function add(x, y) {
+    return x + y;
+}
+
+const multiply = function mult(a,b) {
+    return a * b;
+}
+console.log(multiply(3,7));
+
+function substract(x, y) {
+    return x - y;
+}
+
+const operations = [multiply, add, substract];
+
+for (let func of operations) {
+    console.log(func(5,2));
+}
+
+const functions = {
+    m: multiply,
+}
+
+function callNTimes(func, N) {
+    for (let i = 0; i < N; i++) {
+        func();
+    }
+}
+
+function cry() {
+    console.log("AAAA");
+}
+
+//callNTimes(cry, 3);
+
+function calculator(op,b) {
+    return function func(x) {
+        return operations[op](x, b);
+    };
+}
+
+const a = calculator(1, 3)
+console.log(a(-5));
+
+function makeHasNFT(NFT) {
+    return function func() {
+        return NFT != null;
+    }
+}
+
+const hasAxie = makeHasNFT("Axie"); 
--- Second_Steps/functions_scope.js
+++
@@ -1,8 +0,0 @@
-function add(x, y) {
-    return x + y;
-}
-
-const square = function (num) {
-    return num * num;
-}
-console.log(square(7));
