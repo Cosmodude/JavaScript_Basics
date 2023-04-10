a068d574fe184a7748bcbb3f7de500f8c278bab2
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-10 22:34:51 +0900

    arrow functions in objects

--- Advanced_steps/Functions_in_Objects.js
+++ Advanced_steps/Functions_in_Objects.js
@@ -1,16 +1,24 @@
 // Functions in objects are called Methods
 const person = {
-    age: 32,
+    age: 22, //32
     name: "Vlad",
     fortune: 0, // 10e10,
     married: true,
-    3: "three",
+    kids: "three",
     bodyfat: 10,
     success () {
         return (this.fortune>10e6 && this.bodyfat<15)
     },
     musclemass: 55,
-    jagged() {return(this.bodyfat<15 && this.musclemass>40)}
+    jagged() { return (this.bodyfat < 15 && this.musclemass > 40) },
+    // arrow functions and this:
+    number_of_kids: () => {
+        console.log(this)  // arrow functions don't change the this state 
+        // so they are not used as objects' methods
+        return this.kids
+    }
 };
 
 console.log(person.success())
+console.log(person.number_of_kids())
+
