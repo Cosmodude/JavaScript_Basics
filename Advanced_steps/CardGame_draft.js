d31ecdd8dc0567418e0a669fc5627e1da749ef9d
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-10 22:35:50 +0900

    you can create card game out of this

---
+++ Advanced_steps/CardGame_draft.js
@@ -0,0 +1,33 @@
+const deck = {
+    deck: [],
+    drawnCards: [],
+    suits: ["hearts", "spades", "diamonds", "clubs"],
+    values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+    makeNewDeck () {
+        const { suits, values, deck } = this;  // destructuring
+        deck = [];
+        for (let value of values.split(",")) {
+            for (let suit of suits) {
+                deck.push({
+                    suit,
+                    value
+                })
+            }
+        }
+    },
+    drawCard() {
+        const card = this.deck.pop();
+        this.drawnCards.push(card);
+        return card;
+    },
+    drawManyCards(number) {
+        const cards = [];
+        for (let i = 0; i < number; i++){
+            cards.push(this.drawCard());
+        }
+        return cards;
+    },
+    Shuffle() {
+        
+    }
+}
