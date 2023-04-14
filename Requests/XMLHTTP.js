1321427192cd3fe7ef730b238432266eddcf76fd
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-14 16:52:11 +0900

    very messy

--- Requests/XMLHTTP.js
+++ Requests/XMLHTTP.js
@@ -3,12 +3,22 @@ const firstRequest = new XMLHttpRequest();
 const url = 'https://swapi.dev/api/';
 
 firstRequest.addEventListener('load', function ()  {
-    console.log('works');
+    console.log('first works');
     const respText = JSON.parse(this.responseText);
-    console.log(respText);
-    for (let film of respText.results) {
+    const starshipURL = respText.results[0].starships[0];
+    const starshipReq = new XMLHttpRequest();
+    starshipReq.addEventListener('load', function () {
+        const starship = JSON.parse(this.responseText);
+        console.log(starship);
+    });
+    starshipReq.addEventListener('error', function (e) {
+        console.log("Eror", e);
+    });
+    starshipReq.open("GET", starshipURL);
+    starshipReq.send();
+    /*for (let film of respText.results) {
         console.log(film.title);
-    }
+    }*/
 })
 firstRequest.addEventListener('error', () => {
     console.log('error!');
