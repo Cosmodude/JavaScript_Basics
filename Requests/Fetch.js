1f44384199360f88011dcc5585e6ff45e9506d3f
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-18 09:53:22 +0900

    basic fetch studied

--- Requests/Fetch.js
+++ Requests/Fetch.js
@@ -1,18 +1,34 @@
 const url = 'https://swapi.dev/api/';
 
-fetch(url + "filmssss/")
+fetch(url + "films/")
     .then((response) => {
         if (!response.ok) {
             throw new Error(`Status code Error ${response.status}`);
         }
-            console.log(response);
-            response.json().then((data) => {
-                for (let film of data.results) {
-                    console.log(film.title);
-                }
-            })
+        console.log(response);
+        return response.json();
+    })
+    .then((data) => {
+        console.log("Fetched films");
+        for (let film of data.results) {
+            console.log(film.title);
+            let heroURL = film.characters[0];
+            console.log(heroURL);
+            return fetch(heroURL);
+        }
+    })
+    .then((hero) => {
+        if (!hero.ok) {
+            throw new Error(`Status code Error ${response.status}`);
+        }
+        return hero.json();
+    })
+    .then((character) => {
+        console.log("Fetched main character:");
+        console.log(character.name);
     })
     .catch((err) => {  // will work only if call is impossible
         console.log("error!", err);
-})
+    })  // No Nesting!!!
+// Still code is repeated
 
