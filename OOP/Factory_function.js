5a07f6999117799a793e83021547106cb87d6218
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-19 22:48:11 +0900

    a look into factory functions

---
+++ OOP/Factory_function.js
@@ -0,0 +1,11 @@
+function makeColor(r,g,b){
+    const color = {};
+    color.r = r;
+    color.g = g;
+    color.b = b;
+    color.rgb = function () {
+        const { r, g, b } = this;
+        return `rgb${r}, ${g}, ${b}`;
+    }
+    return color;
+}
