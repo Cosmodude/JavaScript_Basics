88b351cd28f28652feb31e32204b2c8fbd29257b
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-20 10:22:50 +0900

    FF problem

--- OOP/Factory_function.js
+++ OOP/Factory_function.js
@@ -1,11 +1,20 @@
-function makeColor(r,g,b){
+// Creates and returns the object
+// objects follow same pattern 
+function makeColor(r, g, b) {  // creates color
     const color = {};
     color.r = r;
     color.g = g;
     color.b = b;
-    color.rgb = function () {
+    color.rgb = function () {  // defines for every individual object
         const { r, g, b } = this;
         return `rgb${r}, ${g}, ${b}`;
     }
     return color;
 }
+
+const color1 = makeColor(0, 0, 0);
+const color2 = makeColor(255, 255, 255);
+
+console.log("color.rgb", color1.rgb === color2.rgb);
+console.log("string.slice", "hi".slice === "bye".slice)
+
